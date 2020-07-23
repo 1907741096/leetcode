84355c689c1a2ff445aa09e73816ae2208be6e27
wangzhenyuan <wangzhenyuan@bilibili.com>
2020-07-23 10:45:56 +0800

    7.23

--- p64.go
+++ p64.go
@@ -1,10 +1,26 @@
 package main
 
-
-func sumNums(n int) int {
-	if n == 1 {
-		return 1
+func minPathSum(grid [][]int) int {
+	for i := 0; i < len(grid); i ++ {
+		for j := 0; j < len(grid[i]); j ++ {
+			if i == 0 && j == 0 {
+				continue
+			} else if i == 0 {
+				grid[i][j] += grid[i][j - 1]
+			} else if j == 0 {
+				grid[i][j] += grid[i - 1][j]
+			} else {
+				grid[i][j] += min(grid[i][j - 1], grid[i - 1][j])
+			}
+		}
 	}
-	return n + sumNums(n-1)
+	return grid[len(grid) - 1][len(grid[0]) - 1]
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
 }
 
