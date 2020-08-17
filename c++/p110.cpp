226a6180b8b1133c2ab3d4526903fa1bdff56f25
wzy <1907741096@qq.com>
2020-08-17 17:24:33 +0800

    c++

---
+++ c++/p110.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+#include <cstddef>
+#include <utility>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        if (getDepth(root) == -1) return false;
+        return true;
+    }
+    int getDepth(TreeNode* root) {
+        if (root == NULL) {
+            return 0;
+        }
+        int left = getDepth(root->left);
+        int right = getDepth(root->right);
+        if (left == -1 || right == -1 || abs(left - right) > 1) {
+            return -1;
+        }
+        return max(left, right) + 1;
+    }
+};
