e7ea105b93f1d660b3354897832615671cee9301
wzy <1907741096@qq.com>
2020-08-28 10:49:48 +0800

    8.28

--- golang/p491.go
+++ golang/p491.go
@@ -2,31 +2,40 @@ package main
 
 import "strconv"
 
+type t struct {
+	value []int
+	str string
+}
+
 func findSubsequences(nums []int) [][]int {
-	arr := [][]int{}
+	arr := []t{}
 	for i := 0; i < len(nums); i ++ {
 		length := len(arr)
 		for j := 0; j < length; j ++ {
-			if arr[j][len(arr[j]) - 1] <= nums[i] {
-				newArr := make([]int, len(arr[j]) + 1)
-				copy(newArr, arr[j])
-				newArr[len(newArr) - 1] = nums[i]
-				arr = append(arr, newArr)
+			if arr[j].value[len(arr[j].value) - 1] <= nums[i] {
+				newArr := make([]int, len(arr[j].value))
+				copy(newArr, arr[j].value)
+				newT := t{
+					value: append(newArr, nums[i]),
+					str:   arr[j].str + strconv.Itoa(nums[i]) + ",",
+				}
+				arr = append(arr, newT)
+			} else {
+				break
 			}
 		}
-		arr = append(arr, []int{nums[i]})
+		arr = append(arr, t{
+			value: []int{nums[i]},
+			str:   strconv.Itoa(nums[i]),
+		})
 	}
 	res := [][]int{}
 	hashSet := map[string]bool{}
 	for _, a := range arr {
-		s := ""
-		for _, i := range a {
-			s += strconv.Itoa(i) + ","
+		if len(a.value) != 1 && !hashSet[a.str] {
+			res = append(res, a.value)
+			hashSet[a.str] = true
 		}
-		if len(a) != 1 && !hashSet[s]{
-			res = append(res, a)
-		}
-		hashSet[s] = true
 	}
 	return res
 }
