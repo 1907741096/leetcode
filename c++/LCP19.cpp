670cb749bff395841c4a0955a47f7c87663fc829
wzy <1907741096@qq.com>
2020-10-01 11:26:07 +0800

    动态规划

---
+++ c++/LCP19.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+	int minimumOperations(string leaves) {
+		int n = leaves.size();
+		int a = (leaves[0] == 'y');
+		int b, c = INT_MAX;
+		for (int i = 1; i < n; ++i) {
+			int isRed = (leaves[i] == 'r');
+			int isYellow = (leaves[i] == 'y');
+			if (i >= 2) {
+				c = min(b, c) + isYellow;
+			}
+			b = min(a, b) + isRed;
+			a = a + isYellow;
+		}
+		return c;
+	}
+};
