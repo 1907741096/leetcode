f9de1b06227d1f7652c4c6cd1bdab186d3200f48
wzy <1907741096@qq.com>
2020-10-15 11:30:44 +0800

    10.15

---
+++ c++/p24.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        ListNode* node = head;
+        int swap;
+        while (node != nullptr && node->next != nullptr) {
+            swap = node->val;
+            node->val = node->next->val;
+            node->next->val = swap;
+            node = node->next->next;
+        }
+        return head;
+    }
+};
