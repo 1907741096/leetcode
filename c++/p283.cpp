9f30ad839db3c7f2dd277e12970313fae48851ac
wzy <1907741096@qq.com>
2020-11-20 10:22:32 +0800

    双指针

--- c++/p283.cpp
+++ c++/p283.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 	void moveZeroes(vector<int>& nums) {
-		int numIndex = 0, zeroIndex = 0;
+		int temp, numIndex = 0, zeroIndex = 0;
 		while (numIndex < nums.size() && zeroIndex < nums.size()) {
 			if (nums[zeroIndex] == 0) {
 				if (zeroIndex > numIndex) {
@@ -9,7 +9,9 @@ public:
 				}
 				while (++numIndex < nums.size()) {
 					if (nums[numIndex] != 0) {
-						swap(nums[numIndex], nums[zeroIndex]);
+						temp = nums[numIndex];
+						nums[numIndex] = nums[zeroIndex];
+						nums[zeroIndex] = temp;
 						zeroIndex++;
 						break;
 					}
