606c89d14b5b800150839ae6b53ec95d68415081
wzy <1907741096@qq.com>
2021-01-26 16:00:55 +0800

    枚举哈希表

---
+++ c++/p1128.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+	int numEquivDominoPairs(vector<vector<int>>& dominoes) {
+		vector<int> nums(100);
+		int num, res = 0;
+		for (int i = 0; i < dominoes.size(); i ++) {
+			num = dominoes[i][0] > dominoes[i][1] ? dominoes[i][0] * 10 + dominoes[i][1] : dominoes[i][1] * 10 + dominoes[i][0];
+			nums[num] ++;
+			res += nums[num];
+		}
+		return res;
+	}
+};
