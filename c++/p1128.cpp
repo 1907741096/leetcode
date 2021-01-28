cc30337ebda4de4dceed9f125112231979f46caa
wzy <1907741096@qq.com>
2021-01-28 17:58:51 +0800

    2021.1.28

--- c++/p1128.cpp
+++ c++/p1128.cpp
@@ -6,7 +6,7 @@ public:
 		for (int i = 0; i < dominoes.size(); i ++) {
 			num = dominoes[i][0] > dominoes[i][1] ? dominoes[i][0] * 10 + dominoes[i][1] : dominoes[i][1] * 10 + dominoes[i][0];
 			nums[num] ++;
-			res += nums[num];
+			res += (nums[num] - 1);
 		}
 		return res;
 	}
